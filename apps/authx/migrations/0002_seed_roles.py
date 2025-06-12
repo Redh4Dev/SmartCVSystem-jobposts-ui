0730ac1d7d101280817bc25379973616bdd92f95
theinarasu <theinarasu91@gmaiol.com>
2025-06-12 21:32:03 +0530

    SVC-1: Update seeder

--- apps/authx/migrations/0002_seed_roles.py
+++ apps/authx/migrations/0002_seed_roles.py
@@ -4,14 +4,14 @@ from django.db import migrations
 def create_default_roles(apps, schema_editor):
     Role = apps.get_model('authx', 'Role')
     Role.objects.bulk_create([
-        Role(role_name='recruiter',  role_description='Can post jobs and manage applicants'),
-        Role(role_name='candidate',  role_description='Seeks and applies for posted positions'),
-        Role(role_name='company_hr', role_description=' Company HR personnel who review and onboard talent'),
+        Role(role_name='Recruiter',  role_description='Can post jobs and manage applicants'),
+        Role(role_name='Candidate',  role_description='Seeks and applies for posted positions'),
+        Role(role_name='Company HR', role_description=' Company HR personnel who review and onboard talent'),
     ], ignore_conflicts=True)  # ignore_conflicts=True so you can re-run safely
 
 def remove_default_roles(apps, schema_editor):
     Role = apps.get_model('authx', 'Role')
-    Role.objects.filter(role_name__in=['recruiter','candidate','company_hr']).delete()
+    Role.objects.filter(role_name__in=['Recruiter','candidate','Company HR']).delete()
 
 class Migration(migrations.Migration):
 
